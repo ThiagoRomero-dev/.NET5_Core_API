1905e60166cd0eb96f1bd29cc48152375c88300b
Thiago Romero <thiagorv169572@gmail.com>
2022-08-09 19:45:32 -0300

    Criacao dos metodos basicos HTTP

---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models
+{
+    public class Filme
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "O campo titulo é obrigatorio")]
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage ="O campo diretor é obrigatorio")]
+        public string Diretor { get; set; }
+        [StringLength(30,ErrorMessage ="O genero nao pode passar de 30 caracteres")]
+        public string Genero { get; set; }
+
+        [Range(1,300, ErrorMessage ="A duração deve ter entre 1 e 300 minutos")]
+        public int Duracao { get; set; }
+
+    }
+}
+    
